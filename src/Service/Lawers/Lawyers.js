1a74f671d92d508c18836f9603dda0780c0593fd
Nada Magdy badr <nadabadr236@gmail.com>
2025-07-23 20:19:54 +0300

    UpdateLawyerFunctions

--- src/Service/Lawers/Lawyers.js
+++ src/Service/Lawers/Lawyers.js
@@ -1,18 +1,13 @@
-import { collection, doc, getDocs, updateDoc } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, updateDoc } from "firebase/firestore";
 import { db } from "../../firebase";
 
-//=====================GetAll lawyers=================================
-
-import { getDocs, collection } from "firebase/firestore";
-import { db } from "../../firebase";
-
+//=====================GetAll lawyers num=================================
 
 export async function getLawyersNumbers() {
   try {
     const snapshot = await getDocs(collection(db, "lawyers"));
     let accepted = 0;
     let pending = 0;
-
 
     snapshot.forEach((doc) => {
       const data = doc.data();
@@ -36,7 +31,6 @@ export async function getLawyersNumbers() {
       pending: 0,
     };
   }
-
 }
 
 //=======================ApproveLawyers=======================================
@@ -52,3 +46,86 @@ export async function ApproveLawyers(lawyerId) {
   }
 }
 //====================getInformationToPendingLawyers============
+export async function getInformationPendingLawyers() {
+  let pendingLawyersList = [];
+
+  try {
+    const snapshot = await getDocs(collection(db, "lawyers"));
+
+    snapshot.forEach((doc) => {
+      const data = doc.data(); 
+
+      if (data.isApproved === false) {
+        pendingLawyersList.push({
+          id: doc.id,
+          name: data.name,
+          email: data.email,
+          idImageUrl: data.idImageUrl,
+          barAssociationImageUrl: data.barAssociationImageUrl,
+          specializations: data.specializations,
+          ...data, 
+        });
+      }
+    });
+
+    return pendingLawyersList;
+
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+}
+//==========================blockPending================
+export async function rejectLawyer(id) {
+  try {
+     const pendingLawyer= doc(collection(db,"lawyers",id));
+     await updateDoc(pendingLawyer,{
+       isApproved: false,
+        messageToLawyer: message,
+     })
+     console.log("توب التوب");
+     
+    }catch(e){
+        console.log(e);
+        
+      }
+}
+//==============================getAcceptedLawyers==================
+export async function getAcceptedLawyers() {
+  let acceptedLawyers=[];
+  try{
+     const snapshot = await getDocs(collection(db, "lawyers"));
+
+    snapshot.forEach((doc) => {
+      const data = doc.data(); 
+
+      if (data.isApproved === true) {
+        acceptedLawyers.push({
+          id: doc.id,
+          name: data.name,
+          email: data.email,
+          idImageUrl: data.idImageUrl,
+          barAssociationImageUrl: data.barAssociationImageUrl,
+          specializations: data.specializations,
+          ...data, 
+        });
+      }
+    });
+
+    return acceptedLawyers;
+
+  }catch(e){
+    console.log("Error",e);
+    
+  }
+  
+}
+//=================delete===========================================
+export async function deleteLawyer(id) {
+  try {
+    await deleteDoc(doc(db, "lawyers", id));
+    console.log(" deleted successfully");
+  } catch (error) {
+    console.error("Error deleting :", error);
+  }
+}
