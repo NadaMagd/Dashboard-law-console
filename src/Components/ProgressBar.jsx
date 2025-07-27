c94b81998498e691f5bb056b5fbf0b3b034510a0
ashraf talaat <ashraf.talaat.2000@gmail.com>
2025-07-27 20:44:29 +0300

    finish

--- src/Components/ProgressBar.jsx
+++ src/Components/ProgressBar.jsx
@@ -2,12 +2,12 @@ export default function ProgressCard({ title, value, total }) {
   const percent = total > 0 ? Math.round((value / total) * 100) : 0;
 
   return (
-    <div className=" w-full  rounded-2xl overflow-hidden text-white shadow-neutral-600 shadow-md p-6 ">
-      <h3 className="text-md font-semibold goldTxt mb-3 text-center">{title}</h3>
-      
-      <div className=" bgSecondary rounded-full h-4 overflow-hidden">
+    <div className=" w-full bgSecondary rounded-2xl overflow-hidden text-white shadow-neutral-600 shadow-md p-6 hover:shadow-lg transition-all duration-200 hover:scale-[1.01]">
+      <h3 className="text-md font-semibold  mb-3 text-center">{title}</h3>
+
+      <div className=" yellowChar rounded-full h-4 overflow-hidden">
         <div
-          className="bgBtn h-full"
+          className="redChar h-full"
           style={{
             width: `${percent}%`,
             transition: "width 0.5s ease-in-out",
@@ -16,7 +16,7 @@ export default function ProgressCard({ title, value, total }) {
         ></div>
       </div>
 
-      <p className="mt-2 text-sm text-right goldTxt">{percent}%</p>
+      <p className="mt-2 text-sm text-right redC">{percent}%</p>
     </div>
   );
 }
--- src/Components/StatCard.jsx
+++ src/Components/StatCard.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 export default function StatCard({ title, value, icon }) {
   return (
-    <div className="rounded-xl flex  items-center justify-between mb-4 overflow-hidden text-white shadow-neutral-600 shadow-md p-6 hover:shadow-lg transition-all duration-200 w-full h-full">
+    <div className="rounded-xl flex bgSecondary items-center justify-between mb-4 overflow-hidden text-white shadow-neutral-600 shadow-md p-6 hover:shadow-lg duration-200 w-full h-full transition-transform hover:scale-[1.01]">
       
-        <h3 className="text-md  font-semibold ">{title}</h3>
-        <div className=" flex gap-3  items-center text-3xl font-bold mt-2">
+        <h3 className="text-md  font-semibold">{title}</h3>
+        <div className=" flex gap-3 items-center text-3xl font-bold mt-2">
           {value}
           {icon}
         </div>
